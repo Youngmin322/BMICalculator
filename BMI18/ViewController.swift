d8ed099aa7cc6b205f9e80d1b8b2cfb956ec3dbe
Youngmin322 <choyoungmin0322@gmail.com>
2023-11-21 10:17:20 +0900

    Initial Commit

---
+++ BMI18/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI18
+//
+//  Created by 조영민 on 11/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
